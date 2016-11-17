0d13bd5df2b7f38aeb304b6d1729b2c97cd6630f
Supriya Gole <supriyag@thoughtworks.com>
2016-11-17 23:18:58 +0530

    Supriya- Implement different line charts with different interpolation technique for exercise 7.

--- Exercise-7/scripts/visualize.js
+++ Exercise-7/scripts/visualize.js
@@ -10,13 +10,14 @@ var data = [
     {x: 9, y: 2}
 ];
 
-var addLineChart = function (lineGroup, valueType) {
+var addLineChart = function (lineGroup, valueType, title) {
+    lineGroup.append('text').text(title);
     lineGroup.append('path')
         .datum(data)
-        .attr('d', valueType);
+        .attr('d', valueType)
 };
 
-var translate = function (x,y) {
+var translate = function (x, y) {
     return 'translate(' + x + ', ' + y + ')';
 };
 
@@ -37,12 +38,13 @@ var addCirclesToEdgeOfLine = function (group, valueType) {
         .attr('r', 3);
 };
 
-var addAxis = function (axis, x, y) {
-    d3.select('svg').append('g')
-        .attr('transform', translate(x,y))
+var addAxis = function (axis, x, y, title) {
+    d3.select('.'+title).append('g')
+        .attr('transform', translate(x, y))
         .call(axis);
 };
-var generateLineScale = function (valueType, shiftBy) {
+
+var generateLineScale = function (valueType, shiftBy, curveType) {
     return d3.line()
         .x(function (d) {
             return xScale(d.x / 10);
@@ -51,15 +53,34 @@ var generateLineScale = function (valueType, shiftBy) {
             if (valueType)
                 return yScale(Math.sin(d.x) / 10 + shiftBy);
             return yScale(d.y / 10);
-        });
+        })
+        .curve(curveType)
 };
 
 var xScale, yScale;
 
-var visualize = function () {
+var showAllCharts = function () {
+    var interpolationTechniques = [
+        {curve:d3.curveLinear,title:'curveLinear'},
+        {curve:d3.curveLinearClosed,title:'curveLinearClosed'},
+        {curve:d3.curveStep,title:'curveStep'},
+        {curve:d3.curveBasis,title:'curveBasis'},
+        {curve:d3.curveBundle.beta(0.7),title:'curveBundle'},
+        {curve:d3.curveCardinalClosed,title:'curveCardinalClosed'},
+        {curve:d3.curveCatmullRom.alpha(0.25),title:'curveCatmullRom1'},
+        {curve:d3.curveCatmullRom,title:'curveCatmullRom2'}
+    ];
+
+    interpolationTechniques.forEach(function (technique) {
+       showLineChart(technique);
+    });
+
+};
+
+var showLineChart = function (technique) {
     const MARGIN = 30;
-    const HEIGHT = 750;
-    const WIDTH = 1000;
+    const HEIGHT = 500;
+    const WIDTH = 500;
     const shiftBy = 0.5;
 
     const INNERWIDTH = WIDTH - (2 * MARGIN);
@@ -74,24 +95,27 @@ var visualize = function () {
     var svg = d3.select('.container')
         .append('svg')
         .attr('height', HEIGHT)
-        .attr('width', WIDTH);
+        .attr('width', WIDTH)
+        .classed(technique.title,true);
 
-    addAxis(xAxis, MARGIN, HEIGHT - MARGIN);
-    addAxis(yAxis, MARGIN, MARGIN);
+    addAxis(xAxis, MARGIN, HEIGHT - MARGIN, technique.title);
+    addAxis(yAxis, MARGIN, MARGIN, technique.title);
 
-    var lineScale = generateLineScale();
-    var lineWithSinScale = generateLineScale(Math.sin, shiftBy);
+    var lineScale = generateLineScale(null,null,technique.curve);
+    var lineWithSinScale = generateLineScale(Math.sin, shiftBy,technique.curve);
 
     var lineGroup = svg.append('g').classed('lineGroup', true);
-    addLineChart(lineGroup, lineScale);
+    addLineChart(lineGroup, lineScale,technique.title);
     addCirclesToEdgeOfLine(lineGroup);
 
-    var sinGroup = svg.append('g').classed('lineGroup', true);
-    addLineChart(sinGroup, lineWithSinScale);
+    var sinGroup = svg.append('g').classed('lineGroupWithSin', true);
+    addLineChart(sinGroup, lineWithSinScale,technique.title);
     addCirclesToEdgeOfLine(sinGroup, Math.sin);
 
     d3.selectAll('.lineGroup')
-        .attr('transform', translate(MARGIN,MARGIN));
+        .attr('transform', translate(MARGIN, MARGIN));
+    d3.selectAll('.lineGroupWithSin')
+        .attr('transform', translate(MARGIN, MARGIN));
 };
 
-window.onload = visualize;
+window.onload = showAllCharts;
